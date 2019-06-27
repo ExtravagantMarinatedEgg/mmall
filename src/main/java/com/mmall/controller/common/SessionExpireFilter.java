e59894dd45fe382c35e4e6baac0e19ca00e38d74
Tong <m13302061411@163.com>
2019-06-27 15:42:22 +0800

    拦截器配置

--- src/main/java/com/mmall/controller/common/SessionExpireFilter.java
+++ src/main/java/com/mmall/controller/common/SessionExpireFilter.java
@@ -20,18 +20,18 @@ public class SessionExpireFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-//        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-//        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
-//        if (StringUtils.isNotEmpty(loginToken)) {
-//            //判断loginToken是否为空或“”
-//            String userJsonStr = RedisShardedPoolUtil.get(loginToken);
-//            User user = JsonUtil.string2Obj(userJsonStr, User.class);
-//            if (user != null) {
-//                //如果user不为空，则重置session的时间，即调用expire命令
-//                RedisShardedPoolUtil.expire(loginToken, Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
-//            }
-//        }
-//        filterChain.doFilter(servletRequest, servletResponse);
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
+        if (StringUtils.isNotEmpty(loginToken)) {
+            //判断loginToken是否为空或“”
+            String userJsonStr = RedisShardedPoolUtil.get(loginToken);
+            User user = JsonUtil.string2Obj(userJsonStr, User.class);
+            if (user != null) {
+                //如果user不为空，则重置session的时间，即调用expire命令
+                RedisShardedPoolUtil.expire(loginToken, Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
+            }
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 
     @Override
